3130a59807303c89c42befda1a245a0b44510b92
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-03 22:00:19 +0300

    Add a script to calculate average age

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from seed import connect_db
+
+
+def stream_user_ages():
+    """Prints average  ages from the database."""
+    with connect_db() as db:
+        cursor = db.cursor()
+        cursor.execute("SELECT age FROM user_data;")
+        for age in cursor:
+            yield age[0]
+
+
+def average_ages():
+    """Prints average ages from the database."""
+    total_age = 0
+    count = 0
+    for age in stream_user_ages():
+        total_age += age
+        count += 1
+    if count == 0:
+        return 0
+    return total_age / count
