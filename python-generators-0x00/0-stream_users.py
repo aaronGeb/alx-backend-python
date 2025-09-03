e82ca9cb58dd0358d5c3a680c39a00f3a891b1d2
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-03 21:13:10 +0300

    Add a sql query using python generator

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def stream_users():
+    """Stream all users in the database"""
+    db = mysql.connector.connect(
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DATABASE"),
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+    for row in cursor.fetchall():
+        yield row
+    cursor.close()
+    db.close()
+
+
