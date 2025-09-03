ab8df42c9ce4a94a35cdc5afa8cadcfbb7c82c2f
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-03 21:38:44 +0300

    Add data stream and data processing

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from seed import connect_db
+
+
+def stream_users_in_batches(batch_size):
+    """Generator function to yield users in batches"""
+
+    cursor = connect_db.cursor()
+    cursor.execute("SELECT * FROM user_data")
+    while True:
+        rows = cursor.fetchmany(batch_size)
+        if not rows:
+            break
+        for row in rows:
+            yield row
+
+def batch_processing(batch_size):
+    """Generator function to yield users in batches"""
+    cursor = connect_db.cursor()
+    cursor.execute("SELECT * FROM user_data WHERE age > 25")
+    while True:
+        rows = cursor.fetchmany(batch_size)
+        if not rows:
+            break
+        yield rows
