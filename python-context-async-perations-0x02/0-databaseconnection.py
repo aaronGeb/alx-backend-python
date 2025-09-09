cb2f1d4f77a403b8524e1da6d69841eccdd18f8e
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-09 20:57:33 +0300

    Add database connection script

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sqlite3
+
+class DatabaseConnection:
+    """A simple database connection context manager."""
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.connection = None
+        self.cursor = None
+
+    def __enter__(self):
+        # open connection
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        print("Connection established.",self.db_name)
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.connection:
+          self.connection.commit()
+          self.connection.close()
+          print("Connection closed.")
+        if exc_type:
+            print(f"An error occurred: {exc_value}")
+        return False  # Propagate exception if any
+
+
+if __name__ == "__main__":
+    db_name = "example.db"
+    with DatabaseConnection(db_name) as cursor:
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        print("query result:")
+        for row in rows:
+            print(row)
