8b48cf32b359f175edac889a396879aebbf04926
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-16 13:20:49 +0300

    Add mock test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,8 +36,9 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     """TestGetJson class to test get_json function."""
+
     @parameterized.expand(
-      [
+        [
             ("http://example.com", {"payload": True}),
             ("http://holberton.io", {"payload": False}),
         ]
@@ -59,6 +60,7 @@ class TestMemoize(unittest.TestCase):
 
     def test_memoize(self):
         """Test that the memoize decorator caches the result of a method."""
+
         class TestClass:
             def a_method(self):
                 return 42
@@ -67,11 +69,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_a_method:
-            test_instance = TestClass()
-            result1 = test_instance.a_property
-            result2 = test_instance.a_property
-            mock_a_method.assert_called_once()
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
-            self.assertIs(result1, result2)
+    with patch.object(TestClass, "a_method", return_value=42) as mock_a_method:
+        test_instance = TestClass()
+        self.assertEqual(test_instance.a_property, 42)
+        self.assertEqual(test_instance.a_property, 42)
