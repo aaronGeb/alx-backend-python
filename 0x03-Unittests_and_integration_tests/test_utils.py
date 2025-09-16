5ceef826ace7b3af21105b60c0c52a61fde44277
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-16 13:12:48 +0300

    Add mock test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """Unittests for utils module.
 """
 import unittest
-import unitest.mock import patch, Mock
+from unitest.mock import patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
@@ -33,19 +33,18 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
+
 class TestGetJson(unittest.TestCase):
     """TestGetJson class to test get_json function."""
     @parameterized.expand(
       [
-        
             ("http://example.com", {"payload": True}),
             ("http://holberton.io", {"payload": False}),
         ]
     )
-    #@patch("utils.requests.get")
+    @patch("utils.requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
         """Test that the function returns the expected payload."""
-      
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
@@ -53,6 +52,7 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
 
 class TestMemoize(unittest.TestCase):
     """TestMemoize class to test memoize decorator."""
