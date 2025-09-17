75d24b1821d94ce68e0eaff3971a5f0d71abc57e
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-17 15:43:10 +0300

    Add a serializers script

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,33 @@
+from rest_framwork import serializers
+from .models import Chat, Message, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "user_id",
+            "first_name",
+            "last_name",
+            "email",
+            "phone_number",
+            "role",
+            "created_at",
+        ]
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ["message_id", "sender", "content", "timestamp"]
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Chat
+        fields = ["chat_id", "participants", "messages", "created_at"]
