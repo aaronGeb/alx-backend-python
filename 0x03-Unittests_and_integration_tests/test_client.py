ee6a622517f642ff8c99e7500184b6e6c3128aa1
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-20 14:21:26 +0300

    fix pycodestyle valdation error

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -71,6 +71,8 @@ class TestGithubOrgClient(unittest.TestCase):
             [
                 ({"license": {"key": "my_license"}}, "my_license", True),
                 ({"license": {"key": "apache-2.0"}}, "my_license", False),
+                ({"license": None}, "my_license", False),
+                ({}, "my_license", False)
             ]
         )
         def test_has_license(self, repo, license_key, expected):
