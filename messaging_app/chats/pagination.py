69012164d9e461b8704eff666b48ed47a628b2fc
Aaron Gebremariam <aarongebremariam.94@gmail.com>
2025-09-26 13:37:34 +0300

    Add auth and permission to chat app

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -7,5 +7,11 @@ class MessagePagination(PageNumberPagination):
     Allows client to override with ?page_size=... up to 100 max.
     """
     page_size = 20
-    page_size_query_param = "page_size"
-    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            "count": self.page.paginator.count,
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+            "results": data,
+        })
